3d3abef1e4668c10e816ed07c957bb20c8a972d4
Michael S Collins <mcollins@barracuda.com>
2021-05-20 17:29:35 -0700

    Fix

--- aws/powershell/list-waf-images.ps1
+++ aws/powershell/list-waf-images.ps1
@@ -1,6 +1,6 @@
 $wafs = @{}
 $regions=aws ec2 describe-regions | ConvertFrom-Json
-foreach ($region in $regions) {
-    $regionwafs = aws ec2 describe-images --region $region.Regions.RegionName --filters "Name=name,Values=CudaW*fw*" | ConvertFrom-Json
+foreach ($region in $regions.Regions.RegionName) {
+    $regionwafs = aws ec2 describe-images --region $region --filters "Name=name,Values=CudaW*fw*" | ConvertFrom-Json
     $wafs[$region.Regions.RegionName] = $regionwafs.Images
 }
